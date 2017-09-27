c6e25113ff3d279be015997a0b2bac7ea075dc16
JooZh <476506353@qq.com>
2017-09-27 12:51:20 +0800

    2017-9-27

--- jQplugins/particles.param.js
+++ jQplugins/particles.param.js
@@ -1,16 +1,13 @@
 /* -----------------------------------------------
 /* How to use? : Check the GitHub README
 /* ----------------------------------------------- */
-
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
 */
-
 /* Otherwise just put the config content (json): */
-
 particlesJS('particles',
   {
     "particles": {
@@ -130,5 +127,4 @@ particlesJS('particles',
       "background_size": "cover"
     }
   }
-
 );
